19f1136ea9e3df36b699ac7276c9f67ca94c5c42
Brian Illes <brianrilles@gmail.com>
2019-04-08 12:44:09 -0700

    adds repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,9 +2,22 @@ const { succeed, fail, repair, get } = require('./enhancer.js');
 
 describe('enhancer.js', () => {
   describe('enhancer', () => {
-    // describe('enhancer.succeed', () => {});
-    // describe('enhancer.fail', () => {});
-    // describe('enhancer.repair()', () => {});
-    // describe('enhancer.get', () => {});
+    // describe('enhancer.succeed()', () => {});
+    // describe('enhancer.fail()', () => {});
+    describe('enhancer.repair()', () => {
+      it('should accept an item object and return a new item with the durability restored to 100.', () => {
+        expect(repair({ name: 'a', durability: 10, enhancement: 10 })).toEqual({
+          name: 'a',
+          durability: 100,
+          enhancement: 10
+        });
+        expect(repair({ name: 'b', durability: 99, enhancement: 10 })).toEqual({
+          name: 'b',
+          durability: 100,
+          enhancement: 10
+        });
+      });
+    });
+    // describe('enhancer.get()', () => {});
   });
 });
